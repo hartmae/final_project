8350161ed099cfc3851a4cf1fa97016df89d80b1
Asher Hartman <asherhartman@Ashers-Laptop.local>
2025-11-23 20:41:24 -0500

    cleanup

--- FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff79447121ea3fb4f15a05b12486ac0c76848e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
